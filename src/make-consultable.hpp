5656f71f36e137cc4a98342fb4bc614dc16692f5
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2015-03-18 09:32:38 -0400

    adding warning as error in compilation flag

--- src/make-consultable.hpp
+++ src/make-consultable.hpp
@@ -60,7 +60,7 @@
            typename ...ATs,                                             \
            typename ...BTs>                                             \
   R _consult_method(R(_member_type::*function)(ATs...),                 \
-                    BTs ...args) const {                                \
+                    BTs ...) const {                                    \
     static_assert(std::is_const<decltype(function)>::value,             \
                   "consultation is available for const methods only");  \
     return R();  /* for syntax only since assert should always fail */  \
@@ -69,7 +69,7 @@
   template<typename ...ATs,                                             \
            typename ...BTs>                                             \
   void _consult_method(void(_member_type::*function)(ATs...),           \
-                       BTs ...args) const {                             \
+                       BTs ...) const {                                 \
     static_assert(std::is_const<decltype(function)>::value,             \
                   "consultation is available for const methods only");  \
   }
